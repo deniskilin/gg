3aca6af2f672003a91465457b3e909af74bcfc06
DEN <cyberultrapro@gmail.com>
2023-01-18 21:42:24 +0300

    add hello bro den

---
+++ examples666/ex006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input username: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "den")
+{
+    Console.WriteLine("Hello BRO");
+}
+else
+{
+    Console.WriteLine("Hello defolt user");
+}
+
